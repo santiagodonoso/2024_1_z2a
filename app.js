95b6c638a4e80376313778b8c7e57c0dbc228331
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-01 12:59:50 +0100

    first test

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 function show_full_name(){
-   
     let name = document.querySelector("#user_name").value
     let last_name = document.querySelector("#last_name").value
     let nickname = document.querySelector("#nickname").value
-    nickname = "<strong>" + nickname + "</strong>"
+    nickname = "<strong style='font-size: 3rem'>" + nickname + "</strong>"
     let full_name = name + " " + last_name + " " + nickname
     console.log(full_name)
     document.querySelector("div").innerHTML = full_name
-
 }
-
 
 
 
