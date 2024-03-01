c6cd3522ef2789cb7202fa3bb023287084b2ed68
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-01 11:40:46 +0100

    first test

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-let name = "Santiago"
+// let is a variable that can be changed
+let name = "xxxxxx"
+name = "Santiago" 
+let last_name = "Donoso"
+
+let full_name = name + " " + last_name
+
+console.log(full_name)
+
+
+
+
+
+
+
+
 
