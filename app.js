1614a3dfa0cca005a7c24475eaafe1f55f41858f
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-04 10:54:04 +0100

    ...

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ const yellow_light = document.querySelector("#yellow")
 const red_light = document.querySelector("#red")
 const green_light = document.querySelector("#green")
 
-setTimeout(function(){
-    yellow_light.classList.remove("off")
-    red_light.classList.add("off")
-}, 1000)
+function step_one(){
+   red_light.classList.remove("off")
+   yellow_light.classList.add("off")
+   green_light.classList.add("off")
+}
+function step_two(){
+   red_light.classList.remove("off")
+   yellow_light.classList.remove("off")
+   green_light.classList.add("off")
+}
+function step_three(){
+   red_light.classList.add("off")
+   yellow_light.classList.add("off")
+   green_light.classList.remove("off")
+}
+function step_four(){
+   red_light.classList.add("off")
+   yellow_light.classList.remove("off")
+   green_light.classList.add("off")
+}
 
 
-setTimeout(function(){
-    yellow_light.classList.add("off")
-    green_light.classList.remove("off")
-}, 2000)
+
 
 
 
