f2c6f00a3e674001d13f57a1a0759a656706ecbc
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-04 10:06:47 +0100

    ...

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 setTimeout(function(){
     const yellow_light = document.querySelector("#yellow")
     yellow_light.classList.remove("off")
+    const red_light = document.querySelector("#red")
+    red_light.classList.add("off")
 }, 5000)
 
 
