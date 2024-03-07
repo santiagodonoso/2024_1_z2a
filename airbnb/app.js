73b134d375c5ff636e617c703df3f0af24255e2c
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-07 10:25:37 +0100

    ...

--- airbnb/app.js
+++ airbnb/app.js
@@ -1,3 +1,55 @@
+// Exercise:
+// Create a JSON object showing data about you.
+// Give a varaiable name to the object "person"
+// Show in the key values:
+// name, last name, age, city where you live
+let person = {
+    "name" : "A",
+    "last_name" : "B",
+    "age" : 10,
+    "city" : "Køge",
+}
+
+
+
+
+/*
+// Object or JSON object
+// key : value pairs
+let item_one = {
+    "country" : "Denmark",
+    "image" : "1.png",
+    "price" : 10
+}
+
+console.log(item_one.country)
+console.log(item_one.price)
+
+item_one.city = "Copenhagen"
+
+console.log(item_one)
+
+
+item_one.price = 20
+
+console.log(item_one)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const div_items = document.querySelector("#items")
 // index         0      1       2        3       4
 // array defined by []
@@ -17,5 +69,4 @@ items.forEach( item => {
 })
 
 // beforeend afterend beforebegin afterbegin ````
-
-
+*/
