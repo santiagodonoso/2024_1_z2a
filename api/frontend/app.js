8dbfd34e84456c85e106a23503f8fb0cd0a449e1
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-08 10:08:17 +0100

    ...

--- api/frontend/app.js
+++ api/frontend/app.js
@@ -1,5 +1,8 @@
 async function get_data(){
     console.log(event) // click
     console.log(event.target) // button
-    
+    const conn = await fetch("https://paseho.com/")
+    const data = await conn.text()
+    console.log(data)
+    document.querySelector("#my_name").innerHTML = data
 }
