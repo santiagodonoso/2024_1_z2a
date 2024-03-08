9821e65eb6b2ddf8f5ba20d50a56eb36d0806350
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-08 11:16:08 +0100

    ...

--- api/frontend/app.js
+++ api/frontend/app.js
@@ -1,18 +1,24 @@
 async function get_stock(){
-    console.log(event) // click
-    console.log(event.target) // button
+    // console.log(event) // click
+    // console.log(event.target) // button
     const conn = await fetch("https://paseho.com/stock.php")
     // const data = await conn.text()
     const data = await conn.json() // Convert to array or JSON automatically
-    console.log(data)
+    // console.log(data)
     document.querySelector("#companies").innerHTML = ""
     data.forEach( company => {
-        console.log(company)
+        // console.log(company)
         const div_company = `
         <div>
-           ${company.name} 
+           ${company.name} ${company.quote}
         </div>
         `
         document.querySelector("#companies").insertAdjacentHTML("beforeend", div_company)
     })
 }
+
+setInterval( get_stock , 1000  )
+
+
+
+
