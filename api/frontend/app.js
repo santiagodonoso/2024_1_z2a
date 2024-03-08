085f9a15dd617ea9aad8918ca39209bd8ed6e59d
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-08 10:48:18 +0100

    ...

--- api/frontend/app.js
+++ api/frontend/app.js
@@ -1,21 +1,7 @@
 async function get_data(){
     console.log(event) // click
     console.log(event.target) // button
-    const conn = await fetch("https://paseho.com/")
+    const conn = await fetch("https://paseho.com/stock.php")
     const data = await conn.text()
     console.log(data)
-    document.querySelector("#my_name").innerHTML = data
-    document.querySelector("img").src = data
-}
-
-async function get_one(){
-    const conn = await fetch("https://paseho.com/1.php")
-    const data = await conn.text()
-    console.log(data)
-}
-
-async function get_weather(){
-    const conn = await fetch("https://paseho.com/weather.php")
-    const data = await conn.text()
-    document.querySelector("#weather").innerHTML = data
 }
